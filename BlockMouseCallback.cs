fca4baf3701a69768b63d2b5c458dc91481e0071
shubhamjian <shubhamjain107@yahoo.com>
2023-08-19 19:33:18 +0530

    Panel Maker Task Submission

---
+++ BlockMouseCallback.cs
@@ -0,0 +1,71 @@
+﻿using Rhino;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using System.Linq;
+using System.Collections.Generic;
+using Rhino.Display;
+using System.Drawing;
+
+namespace PanelMaker
+{
+    public class BlockMouseCallback : MouseCallback
+    {
+        private bool DoubleClickEnabled = false;
+        private RhinoObject SelObject;
+
+        
+
+        
+
+        public void BlockContainmentTest(InstanceObject iObj, PickContext picker, ref BoundingBox box)
+        {
+            if (iObj != null)
+            {
+                foreach (var subObj in iObj.GetSubObjects())
+                {
+                    if (!(subObj is InstanceObject))
+                    {
+                        var geo = subObj.Geometry.Duplicate();
+                        var bbox = geo.GetBoundingBox(true);
+                        var bl = picker.PickFrustumTest(bbox, out bool _completeContain);
+                        if (bl)
+                        {
+                            box = bbox;
+                            break;
+                        }
+                        else
+                        {
+                            box = BoundingBox.Unset;
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        var subIobj = subObj as InstanceObject;
+                        BlockContainmentTest(subIobj, picker, ref box);
+                    }
+                }
+            }
+        }
+        
+
+        public RhinoObject SelectObjectsInObjectGroups(RhinoDoc doc)
+        {
+            var rs = new GetPoint();
+            rs.AcceptNothing(true);
+            rs.EnableSnapToCurves(true);
+            rs.SetCommandPrompt("Select Object in Block");
+            //rs.SetCursor(CursorStyle.Default);
+            var result = rs.Get();
+            var obj = rs.PointOnObject();
+            if (obj != null)
+            {
+                return obj.Object();
+            }
+            return null;
+        }
+
+    }
+}
---
+++ PanelMakerPlugin.cs
@@ -0,0 +1,48 @@
+﻿using Rhino;
+using Rhino.Collections;
+using Rhino.FileIO;
+using Rhino.PlugIns;
+using Rhino.UI;
+
+namespace PanelMaker
+{
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class PanelMakerPlugin : Rhino.PlugIns.PlugIn
+    {
+        public PanelMakerPlugin()
+        
+        {
+            Instance = this;
+        }
+
+        ///<summary>Gets the only instance of the PanelMakerPlugin plug-in.</summary>
+        public static PanelMakerPlugin Instance { get; private set; }
+
+        protected override LoadReturnCode OnLoad(ref string errorMessage)
+        {
+            // 3. register UI panels
+            RegisterPanels();
+
+            // disable rhino double click for custom blocks
+            _ = new BlockMouseCallback { Enabled = true };
+
+            // TODO: optimize events, disabled for now because of speed
+            
+
+            return base.OnLoad(ref errorMessage);
+        }
+        private void RegisterPanels()
+        {
+            Panels.RegisterPanel(this, typeof(PanelMaker.UI.Views.MainView), "Plate Generator", System.Drawing.SystemIcons.Application);
+            
+            
+        }
+    }
+}
